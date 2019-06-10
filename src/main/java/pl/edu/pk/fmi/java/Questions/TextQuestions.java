7959b3acc06cbdd0d7c53fc36bb70a87ad247aa7
Tomasz Kacza <kaczatom4@gmail.com>
2019-06-10 17:53:25 +0200

    add field in TextQuestions

--- src/main/java/pl/edu/pk/fmi/java/Questions/TextQuestions.java
+++ src/main/java/pl/edu/pk/fmi/java/Questions/TextQuestions.java
@@ -2,6 +2,11 @@ package pl.edu.pk.fmi.java.Questions;
 
 public class TextQuestions implements Question
 {
+    private int nr;
+    private String body;
+    private String[] answers;
+    private String correct;
+
     @Override
     public String[] get_questtion()
     {
