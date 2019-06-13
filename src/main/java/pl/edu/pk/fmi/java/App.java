57a62f2e286785b3a7ce0e9502aa7dae8707a0e1
Tomasz Kacza <kaczatom4@gmail.com>
2019-06-13 19:25:10 +0200

    add Questions.txt - base add get_Questions and constructor with reading from file

--- src/main/java/pl/edu/pk/fmi/java/App.java
+++ src/main/java/pl/edu/pk/fmi/java/App.java
@@ -1,7 +1,10 @@
 package pl.edu.pk.fmi.java;
 
 
+import pl.edu.pk.fmi.java.Questions.Question;
 import pl.edu.pk.fmi.java.Questions.TextQuestions;
+
+import java.io.IOException;
 
 /**
  * Hello world!
@@ -9,9 +12,15 @@ import pl.edu.pk.fmi.java.Questions.TextQuestions;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
         TextQuestions q = new TextQuestions();
+        String[] a = new String[6];
+        a = q.get_questtion();
+        for (String x: a)
+        {
+            System.out.println(x);
+        }
     }
 }
--- src/main/java/pl/edu/pk/fmi/java/Questions/TextQuestions.java
+++ src/main/java/pl/edu/pk/fmi/java/Questions/TextQuestions.java
@@ -1,5 +1,12 @@
 package pl.edu.pk.fmi.java.Questions;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
 public class TextQuestions implements Question
 {
     private int nr;
@@ -7,20 +14,72 @@ public class TextQuestions implements Question
     private String[] answers;
     private String correct;
 
+    public TextQuestions() throws IOException
+    {
+        this.set_from_file();
+    }
+
     @Override
     public String[] get_questtion()
     {
+        System.out.println("get_questtion");
         /**
+         *
          *  zwraca łańcuchy do labeli w głównym widoku
          *
          */
-        String[] q = new String[5];
+        String[] q = new String[6];
         q[0] = this.body;
-        for (int i = 1; i < 5; i++)
+        for(int i = 1; i < 5; i++)
         {
-            q[i] = answers[i-1];
+            System.out.println(i);
+            q[i] = answers[i - 1];
         }
         q[5] = this.correct;
         return q;
     }
-}
+
+    private void set_from_file() throws IOException
+    {
+        BufferedReader bf = null;
+        try
+        {
+            this.answers = new String[4];
+            String fileName = "src\\main\\java\\pl\\edu\\pk\\fmi\\java\\Questions\\Question.txt";
+
+            bf = new BufferedReader(new FileReader(fileName));
+
+            this.nr = Integer.valueOf(bf.readLine());
+            this.body = bf.readLine();
+            for (int i = 0; i < 4; i++)
+            {
+                this.answers[i] = bf.readLine();
+            }
+            this.correct = bf.readLine();
+
+        }
+        catch (IOException ex)
+        {
+            System.out.println("blad pliku");
+            System.exit(1);
+        }
+        finally
+        {
+            bf.close();
+        }
+        }
+
+        @Override
+        public String toString ()
+        {
+            return "Pytanie \n" +
+                    "{\n" +
+                    "nr=" + nr + "\n" +
+                    "tresc='" + body + '\'' + "\n" +
+                    "odpowiedzi=" + Arrays.toString(answers) + "\n" +
+                    "poprawna='" + correct + '\'' + "\n" +
+                    '}';
+        }
+
+
+    }
