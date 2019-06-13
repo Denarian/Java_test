5762ce92066e5ff3c18952f16e6b37ed5a1fff5a
Tomasz Kacza <kaczatom4@gmail.com>
2019-06-13 17:05:37 +0200

    add get_questtion

--- src/main/java/pl/edu/pk/fmi/java/Questions/TextQuestions.java
+++ src/main/java/pl/edu/pk/fmi/java/Questions/TextQuestions.java
@@ -10,7 +10,17 @@ public class TextQuestions implements Question
     @Override
     public String[] get_questtion()
     {
+        /**
+         *  zwraca łańcuchy do labeli w głównym widoku
+         *
+         */
         String[] q = new String[5];
+        q[0] = this.body;
+        for (int i = 1; i < 5; i++)
+        {
+            q[i] = answers[i-1];
+        }
+        q[5] = this.correct;
         return q;
     }
 }
