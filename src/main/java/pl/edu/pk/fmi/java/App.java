b40e34e361162eb7a491a39538a7a207593f7048
ArturHalat <artur.halat97@gmail.com>
2019-06-15 18:08:01 +0200

    Public answer Lifeline

--- src/main/java/pl/edu/pk/fmi/java/App.java
+++ src/main/java/pl/edu/pk/fmi/java/App.java
@@ -2,6 +2,7 @@ package pl.edu.pk.fmi.java;
 
 import pl.edu.pk.fmi.java.Lifelines.Lifelines_50_50;
 import pl.edu.pk.fmi.java.Lifelines.Lifelines_Phone;
+import pl.edu.pk.fmi.java.Lifelines.Lifelines_Public_answer;
 import pl.edu.pk.fmi.java.PlayerData.MainPlayer;
 
 public class App
@@ -11,9 +12,10 @@ public class App
         System.out.println( "Hello World!" );
         Lifelines_50_50 temp=new Lifelines_50_50();
         Lifelines_Phone temp2=new Lifelines_Phone();
+        Lifelines_Public_answer temp3=new Lifelines_Public_answer();
         String[] temp4=new String[2];
-        temp4=temp2.Lifeline("jeden","dwa","trzy","cztery","dwa");
-        System.out.println(temp4[0]+" "+temp4[1]);
+        temp4=temp3.Lifeline("jeden","dwa","trzy","cztery","cztery");
+        for(int i=0;i<8;i++) System.out.println(temp4[i]);
 
 
     }
--- src/main/java/pl/edu/pk/fmi/java/Lifelines/Lifelines_Public_answer.java
+++ src/main/java/pl/edu/pk/fmi/java/Lifelines/Lifelines_Public_answer.java
@@ -1,19 +1,81 @@
 package pl.edu.pk.fmi.java.Lifelines;
+import java.util.Random;
 
 public class Lifelines_Public_answer implements interfaceToLifelines {
 
     //zmienna informująca czy koło zostało użyte
     private int stat=0;
+    //inicjalizacja generatora liczb
+    private Random generator = new Random();
 
     @Override
-    public int getUseValue() {return this.stat; }
+    public int getUseValue() {return this.stat;}
 
     @Override
     public void ChangeUsed() {this.stat=1; }
 
     @Override
     public String[] Lifeline(String answerA, String answerB, String answerC, String answerD, String rightAnswer) {
-        //tba
-        return new String[0];
-    }
-}
+        //obsługa wyjątków dla argumentów funkcji
+        if(this.stat==1)
+        {
+            throw new RuntimeException("to koło zostało już użyte: ");
+        }
+        if(answerA==answerB || answerA==answerC || answerA==answerD || answerB==answerC ||answerB==answerD || answerC==answerD)
+        {
+            throw new IllegalArgumentException("kilka odpowiedzi jest takich samych: ");
+        }
+        if(rightAnswer!=answerA && rightAnswer!=answerB && rightAnswer!=answerC && rightAnswer!=answerD)
+        {
+            throw new IllegalArgumentException("Poprawna odpowiedz nie jest jedną z podanych: ");
+        }
+        //LOSOWANIE SZANS
+        int a = generator.nextInt(100);
+        int b = generator.nextInt(100-a);
+        int c = generator.nextInt(100-a-b);
+        int d=100-a-b-c;
+        //TABLICA SZANS
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        int [] tabValue={a,b,c,d};
+        //MAX TABLE
+        int max=0,rembi=0;
+        for(int i=0;i<4;i++)
+        {
+            if (tabValue[i]>max) {
+                max = tabValue[i];
+                rembi=i;
+            }
+        }
+
+        if(rembi!=3){
+            int tempindex=tabValue[3];
+            tabValue[3]=tabValue[rembi];
+            tabValue[rembi]=tempindex;
+        }
+
+        if (answerA==rightAnswer)
+        {
+            String[] wynikowa={answerA,Integer.toString(max),answerB,Integer.toString(tabValue[0]),answerC,Integer.toString(tabValue[1]),answerD,Integer.toString(tabValue[2])};
+            return wynikowa;
+        }
+        if (answerB==rightAnswer)
+        {
+            String[] wynikowa={answerB,Integer.toString(max),answerA,Integer.toString(tabValue[0]),answerC,Integer.toString(tabValue[1]),answerD,Integer.toString(tabValue[2])};
+            return wynikowa;
+        }
+        if (answerC==rightAnswer)
+        {
+            String[] wynikowa={answerC,Integer.toString(max),answerA,Integer.toString(tabValue[0]),answerB,Integer.toString(tabValue[1]),answerD,Integer.toString(tabValue[2])};
+            return wynikowa;
+        }
+        if (answerD==rightAnswer)
+        {
+            String[] wynikowa={answerD,Integer.toString(max),answerA,Integer.toString(tabValue[0]),answerB,Integer.toString(tabValue[1]),answerC,Integer.toString(tabValue[2])};
+            return wynikowa;
+        }
+        String []taberror={"ERROR"};
+        return taberror;
+}}
