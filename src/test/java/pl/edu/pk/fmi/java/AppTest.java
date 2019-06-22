df9c2c32e3ae22446d2f0fb156a2169a51710d7e
Tomasz Kacza <kaczatom4@gmail.com>
2019-06-22 11:20:40 +0200

    add test_Lifelines_Phone_status

--- src/test/java/pl/edu/pk/fmi/java/AppTest.java
+++ src/test/java/pl/edu/pk/fmi/java/AppTest.java
@@ -4,17 +4,52 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import pl.edu.pk.fmi.java.Lifelines.Lifelines_Phone;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
      */
+
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void test_Lifelines_Phone_status()
     {
-        assertTrue( true );
+        Lifelines_Phone Lifelines_Phone_Test = new Lifelines_Phone();
+
+
+        Lifelines_Phone_Test.checkstatus(1);
+
+        if (Lifelines_Phone_Test.getUseValue() == 1)
+        {
+            assertTrue(true);
+        } else if (Lifelines_Phone_Test.getUseValue() != 1)
+        {
+            assertTrue(false);
+        }
     }
+
+    public void test_Lifelines_Phone_status()
+    {
+        Lifelines_Phone Lifelines_Phone_Test = new Lifelines_Phone();
+
+        Lifelines_Phone_Test.checkstatus(1);
+
+        if (Lifelines_Phone_Test.getUseValue() == 1)
+        {
+            assertTrue(true);
+        } else if (Lifelines_Phone_Test.getUseValue() != 1)
+        {
+            assertTrue(false);
+        }
+    }
+
+
+
+
+
 }
