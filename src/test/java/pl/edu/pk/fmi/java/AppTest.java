e570cb0c313958657ffc184df719ac543925d42a
ArturHalat <artur.halat97@gmail.com>
2019-06-22 11:43:21 +0200

    Test działania 50_50

--- src/test/java/pl/edu/pk/fmi/java/AppTest.java
+++ src/test/java/pl/edu/pk/fmi/java/AppTest.java
@@ -3,6 +3,7 @@ package pl.edu.pk.fmi.java;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import pl.edu.pk.fmi.java.Lifelines.Lifelines_50_50;
 
 /**
  * Unit test for simple App.
@@ -13,8 +14,18 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void Class50_50RightAnswerTest()
     {
-        assertTrue( true );
+        Lifelines_50_50 temp=new Lifelines_50_50();
+        String[] rozw1=new String[2];
+        int counter =0;
+        for(int i=0;i<1000;i++) {
+            rozw1 = temp.Lifeline("jeden", "dwa", "trzy", "cztery", "a");
+            if(rozw1[0]!=rozw1[1]){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+        if(counter==1000)assertTrue( true );
     }
 }
